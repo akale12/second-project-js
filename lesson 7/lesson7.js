974a8ebc2ebdec8a756d8a7e7b0be8c37012c89a
Khan <unsmiling1337@gmail.com>
2021-04-28 18:51:57 +0600

    obj commit edited

--- lesson 7/lesson7.js
+++ lesson 7/lesson7.js
@@ -2,3 +2,12 @@ const firstname = "Alex",
       lastname = "Ivanov";
       
  console.log(`Hello ${firstname} ${lastname}`);     
+
+
+ const obj = {
+     a: 50
+ };
+
+ obj.a = 25;
+
+ console.log(obj);
