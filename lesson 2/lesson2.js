4b0d8d33bb3ad9c2e5f611b6d8c1a313075ad1a4
Khan <unsmiling1337@gmail.com>
2021-04-28 16:48:12 +0600

    lesson sever file

---
+++ lesson 2/lesson2.js
@@ -0,0 +1,52 @@
+"use strict";
+
+// a = 15; 
+// console.log(a);
+
+let $number = 5;
+
+$number = 10;
+console.log($number);
+
+const leftBorderWidth = 1;    //такой формат называется КемолКейс
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+// leftBorderWidth = 10;
+
+
+console.log(name);
+var name = "Ivan";  //Хостинг, или всплытие переменных
+
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);  //прелесть такого подхода - пока мне не понятен и отличается такой особенностью
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);  //с переменной var это функция работает
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
+
+const cat = "toys";
+
+console.log(`Hello,${cat}`);
---
+++ lesson 3/lesson3.js
@@ -0,0 +1,96 @@
+"use strict";
+
+let number = 4.6;
+
+console.log(4/0);
+console.log('string' * 9);
+
+const persone = "Alex";
+
+const bool = false;
+
+console.log( 10 < 8);
+
+
+console.log('2' === 2);
+
+let halo;
+
+console.log(halo);
+
+
+// console.log(something); //null - это когда вообще ничего не существует.
+
+let und;
+console.log(und);
+
+const $$obj = {
+    name: "John", // ключ = значения ключ это name, значения - это John
+    age: 25,
+    isMarried: false        
+};
+
+// console.log($$obj.name);
+
+// console.log($$obj["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+const $personee = {
+    name: "Khan",
+    age: 22,
+    isMarried: false 
+};
+
+console.log($personee.name);
+
+// console.log($personee["age"]);
+
+
+
+
+
+
+
+
+
+
+// a = 15; 
+// console.log(a);
+
+let $number = 5;
+
+$number = 10;
+console.log($number);
+
+const leftBorderWidth = 1;    //такой формат называется КемолКейс
+
+const obj = {
+    a: 50
+};
+
+obj.a = 10;
+
+console.log(obj);
+
+// leftBorderWidth = 10;
+
+
+console.log(name);
+var name = "Ivan";  //Хостинг, или всплытие переменных
+
+
+// {
+//     let result = 50;
+// }
+
+// console.log(result);  //прелесть такого подхода - пока мне не понятен и отличается такой особенностью
+
+// {
+//     var result = 50;
+// }
+
+// console.log(result);  //с переменной var это функция работает
+
